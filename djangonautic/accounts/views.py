92b27566acfc53b4de5b72f921edc86d9b644125
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-17 12:22:28 +0700

    added logout functionality

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,authenticate,logout
 
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log the user in
             return redirect('articles:list')
     else:        
@@ -18,7 +19,14 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # login the user
+            user = form.get_user()
+            login(request,user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html',{'form':form})
+
+def logout_view(request):
+    if(request.method == 'POST'):
+        logout(request)
+        return redirect('articles:list')
