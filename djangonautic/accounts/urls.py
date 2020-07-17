80ac4e3661cb6a47f70f7f7dee10175f20847523
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-17 11:21:10 +0700

    created user account edited url, views and settings and added template to send back

---
+++ djangonautic/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path,re_path
+
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    re_path(r'^signup/$',views.signup_view,name="signup"),
+]
