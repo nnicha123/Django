2cd8b743ca70f145479d8522084a6ad8d4fcde34
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-17 09:42:24 +0700

    setup urls and articles -> netninja tutorial

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,7 +1,10 @@
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',views.article_list),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_details),
 ]
+urlpatterns += staticfiles_urlpatterns()
